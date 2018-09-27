15b78dae54954feb4e54a1b401ba2a726e18a9a6
Malzik <alexis.heroin@epsi.fr>
2018-09-27 12:37:23 +0200

    Add meta tag

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-	<title>Letitre</title>
+	<title>Site Ben&ALexis (Provisoire)</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" href="style/img/f1.png" />
 </head>
 <body>
 
