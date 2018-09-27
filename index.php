a54c0530d9f1ad5160b8a2d798bf38aac358a4a9
Malzik <alexis.heroin@epsi.fr>
2018-09-27 17:21:01 +0200

    Boostrap + navbar

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/x-icon" href="style/img/f1.png" />
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="style/css/style.css">
 </head>
 <body>
-
+	<?php
+	include 'fonction/function.php';
+		echo headerNav(); 
+	?>
 </body>
 </html>
